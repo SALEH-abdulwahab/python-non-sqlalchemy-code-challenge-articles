fd2495d7270c45e17cffe85425251abb214e2a8f
SALEH-abdulwahab <saleh200228@gmail.com>
2025-08-17 23:49:58 +0300

    cm

--- lib/debug.py
+++ lib/debug.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 import ipdb;
+from author import Author
 
 from classes.many_to_many import Article
 from classes.many_to_many import Author
 from classes.many_to_many import Magazine
-
+a1 = Author("Alice")
+print(a1._name)
 if __name__ == '__main__':
     print("HELLO! :) let's debug :vibing_potato:")
 
